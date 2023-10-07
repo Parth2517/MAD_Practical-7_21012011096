1ff9c4d5fb85191dcf30ae238907e37ff160d1ec
Parth2517 <patelparth88495812@gmail.com>
2023-10-07 13:13:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_7_21012011096/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mad_practical_7_21012011096
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import androidx.core.view.WindowCompat
+import com.example.mad_practical_7_21012011096.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate (savedInstancestate: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstancestate)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initVideoPlayer()
+        binding.btnSwitch.setOnClickListener{
+            Intent(this,WebviewActivity::class.java).also{ startActivity(it) }
+        }
+    }
+    private fun initVideoPlayer(){
+        val mediaController = MediaController(this)
+        val video_path = "android.resource://" + packageName + "/" + R.raw.thestoryoflight
+        val video_uri = Uri.parse(video_path)
+        binding.videoView.setMediaController(mediaController)
+        mediaController.setAnchorView(binding.videoView)
+        binding.videoView.setVideoURI(video_uri)
+        binding.videoView.requestFocus()
+        binding.videoView.start()
+    }
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+    }
+}
---
+++ app/src/main/java/com/example/mad_practical_7_21012011096/WebviewActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mad_practical_7_21012011096
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebSettings
+import androidx.core.view.WindowCompat
+import com.example.mad_practical_7_21012011096.databinding.ActivityWebviewBinding
+
+class WebviewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWebviewBinding
+    override fun onCreate (savedInstancestate: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstancestate)
+
+        binding = ActivityWebviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initWebVideoPlayer()
+        binding.btnSwitch.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
+
+    }
+    private val youtubeld = "6stlCkUDG_s"
+    private fun initWebVideoPlayer(){
+        val webSettings: WebSettings = binding.webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+        binding.webView.loadUrl("https://www.youtube.com/embed/$youtubeld")
+
+    }
+}
